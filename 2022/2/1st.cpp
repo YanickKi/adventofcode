18e67b44b934e3cc5fb2b665c14d483756e1fe04
Yanick Kind <yanick.kind@udo.edu>
2023-02-06 12:48:41 +0100

    lol simon stinkt

--- 2022/2/1st.cpp
+++ 2022/2/1st.cpp
@@ -10,15 +10,19 @@ using namespace std;
 int main () {
   ifstream input; 
   input.open ("input");
-  string one_elf;
-  vector<string> all_elfs;
-  vector<int> calories;
+  string _frist_, _second_;
+  vector<string> __First, __Second;
 
   if(input.is_open()){
-      while(input.good()){
-        getline(input, one_elf);
-        all_elfs.push_back(one_elf);
-    }
+   //   while(input.good()){
+   //     getline(input, one_elf);
+   //     all_elfs.push_back(one_elf);
+   // }
+  
+  while (input >> _frist_ >> _second_) {
+    __First.push_back(_frist_);
+    __Second.push_back(_second_);
+  }
   }
     input.close();
     for(auto i = all_elfs.begin(); i != all_elfs.end(); i++){
