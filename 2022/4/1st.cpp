f902eda05785174a02456472f8f51fb73bca7155
Yanick Kind <yanick.kind@udo.edu>
2023-02-13 12:11:40 +0100

    lol

--- 2022/4/1st.cpp
+++ 2022/4/1st.cpp
@@ -10,15 +10,23 @@ using namespace std;
 int main () {
   ifstream input; 
   input.open ("input");
-  string pair;
+  string pair_temp;
   vector<string> __pair;
+  string temp;
+  vector<string> split;
 
   if(input.is_open()){
       while(input){
-        getline(input, pair);
-        __pair.push_back(pair); 
+        getline(input, pair_temp);
+        __pair.push_back(pair_temp); 
     }
     input.close();
+  }
+
+  for(int pair = 0; pair < __pair.size(); pair++){
+    for(int single = 0; single < __pair[pair].size(); single++){
+      if()
+    }
   }
 //for(auto i = __pair.begin(); i < __pair.end(); i++){
 //  cout << *i << endl;
