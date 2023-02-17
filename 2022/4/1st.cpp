4ea876475be90932d2b7afe4653826ca32fc32f2
Yanick Kind <yanick.kind@udo.edu>
2023-02-17 21:40:57 +0100

    day 4 comptlete

--- 2022/4/1st.cpp
+++ 2022/4/1st.cpp
@@ -11,26 +11,52 @@ int main () {
   ifstream input; 
   input.open ("input");
   string pair_temp;
-  vector<string> __pair;
-  string temp;
-  vector<string> split;
+  vector<string> NUMBERS;
 
   if(input.is_open()){
-      while(input){
-        getline(input, pair_temp);
-        __pair.push_back(pair_temp); 
-    }
+
+  while(getline(input, pair_temp)){
+    size_t prev = 0, pos;
+    while ((pos = pair_temp.find_first_of(" -,", prev)) != std::string::npos)
+      {
+        if (pos > prev)
+            NUMBERS.push_back(pair_temp.substr(prev, pos-prev));
+        prev = pos+1;
+      }
+    if (prev < pair_temp.length())
+        NUMBERS.push_back(pair_temp.substr(prev, std::string::npos));
+  }
+
     input.close();
   }
 
-  //for(int pair = 0; pair < __pair.size(); pair++){
-  //  for(int single = 0; single < __pair[pair].size(); single++){
-  //    if()
-  //  }
-  //}
-for(auto i = __pair.begin(); i < __pair.end(); i++){
-  cout << *i << endl;
-}
+  int count = 0;
+  for(int pos = 3; pos < NUMBERS.size(); pos += 4){
+    if(stoi(NUMBERS[pos]) <= stoi(NUMBERS[pos - 2]) && stoi(NUMBERS[pos-1]) >= stoi(NUMBERS[pos-3])) {
+      count += 1;
+    }
+    if(stoi(NUMBERS[pos]) >= stoi(NUMBERS[pos - 2]) && stoi(NUMBERS[pos-1]) <= stoi(NUMBERS[pos-3])) {
+      count += 1;
+    }
+    if(stoi(NUMBERS[pos]) == stoi(NUMBERS[pos - 2]) && stoi(NUMBERS[pos-1]) == stoi(NUMBERS[pos-3])){
+      count -= 1;
+    }
+  }
+  cout << "count PART 1: " << count << endl;
 
-    return 0;
+
+  //Part 2
+
+  count = 0;
+  for(int pos = 3; pos < NUMBERS.size(); pos += 4){
+    if(stoi(NUMBERS[pos-3]) <= stoi(NUMBERS[pos - 1]) and stoi(NUMBERS[pos-1]) <= stoi(NUMBERS[pos - 2] )
+    or stoi(NUMBERS[pos-1]) <= stoi(NUMBERS[pos - 3]) and stoi(NUMBERS[pos-3]) <= stoi(NUMBERS[pos] )
+    or stoi(NUMBERS[pos])   >= stoi(NUMBERS[pos - 2]) and stoi(NUMBERS[pos-1]) <= stoi(NUMBERS[pos-2]   )
+    or stoi(NUMBERS[pos-2]) >= stoi(NUMBERS[pos])     and stoi(NUMBERS[pos-3]) <= stoi(NUMBERS[pos]   )
+    or stoi(NUMBERS[pos-3]) == stoi(NUMBERS[pos - 1]) and stoi(NUMBERS[pos-2]) == stoi(NUMBERS[pos] )) {
+      count += 1;
+    }
+  }
+  cout << "count PART 1: " << count << endl;
+  return 0;
 }
